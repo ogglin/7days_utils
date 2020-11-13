10b364ec8a1fda1a2a3880bc03b5bee787f61c0e
ares <server.ares@gmail.com>
2020-11-13 15:08:32 +0300

    v0.0.3c fix gen classy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from gen_news import init as init_news
 from gen_classy import init as init_classy
+import re
 
 if __name__ == '__main__':
-    init_news()
-    init_classy()
+    # init_news()
+    # init_classy()
+    pass
